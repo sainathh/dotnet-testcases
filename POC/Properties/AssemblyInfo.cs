d7e3f746722026e4c0bca4ad003af4426a9dd31e
root <root@HS-API-STG.31ju1y2erlyethplc3l44xn3ig.xx.internal.cloudapp.net>
2021-05-25 10:49:40 +0000

    Scusa files added

---
+++ POC/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("POC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("POC")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("733cef89-0935-439f-9abb-3b08392b1d1c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ POC/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+
+namespace POC
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;// Path.GetDirectoryName(Directory.GetCurrentDirectory());
+            IWebDriver driver = new ChromeDriver(path+"\\POC\\Drivers\\");
+            Thread.Sleep(2000);
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            driver.Manage().Window.Maximize();
+            Thread.Sleep(2000);
+        }
+    }
+}
